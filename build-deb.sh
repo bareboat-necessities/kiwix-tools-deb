b4d92a4389ebf446583ca55470e4789b45297f63
mgrouch <mgrushinskiy@gmail.com>
2022-09-04 14:00:31 -0400

    initial

--- build-deb.sh
+++ build-deb.sh
@@ -17,10 +17,11 @@ sudo apt-get -y install meson cmake make git \
  dpkg-dev debhelper devscripts equivs pkg-config \
  libpugixml-dev libmicrohttpd-dev libicu-dev libcurl4-openssl-dev zlib1g-dev aria2
 
-sudo apt-get -y install liblzma-dev libicu-dev libzstd-dev libxapian-dev
+sudo apt-get -y install liblzma-dev libicu-dev libzstd-dev libxapian-dev libcurl4-gnutls-dev
 rm -rf libzim
 git clone --recurse-submodules --remote-submodules https://github.com/openzim/libzim
 cd libzim
+sed -i 's/0.0.0/7.2.0.0/g' debian/changelog
 dpkg-buildpackage -b -d -uc -us
 cd ..
 sudo dpkg -i libzim7_*.deb libzim-dev_*.deb

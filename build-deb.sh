cff91d4404f0a13c8ca173503db1fb7e31c44041
mgrouch <mgrushinskiy@gmail.com>
2022-09-04 12:53:50 -0400

    initial

--- build-deb.sh
+++ build-deb.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -e
 
 sudo apt-get -y install meson cmake make git \
- dpkg-dev debhelper devscripts equivs pkg-config 
+ dpkg-dev debhelper devscripts equivs pkg-config
+
+sudo apt-get install python3-pip virtualenv
+
+virtualenv -p python3 ./ # Create virtualenv
+source bin/activate      # Activate the virtualenv
+
+git clone https://github.com/kiwix/kiwix-build.git
+cd kiwix-build
+pip3 install .
+hash -r # Refresh bash paths
 
 rm -rf kiwix-tools
 git clone https://github.com/kiwix/kiwix-tools/

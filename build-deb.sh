8faaf024cf669e6629e5843c0a00c98fcbbd99a5
mgrouch <mgrushinskiy@gmail.com>
2022-09-04 12:44:20 -0400

    initial

---
+++ build-deb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+sudo apt-get -y install meson cmake make git \
+ dpkg-dev debhelper devscripts equivs pkg-config
+
+rm -rf kiwix-tools
+git clone https://github.com/kiwix/kiwix-tools/
+
+cd kiwix-tools
+dpkg-buildpackage -b -d -uc -us
+
+
